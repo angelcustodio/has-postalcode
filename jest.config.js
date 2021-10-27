718c6db294862c4d13038b5e8e590c065fbbf0d7
Angel Custodio <ancude@gmail.com>
2021-10-27 16:12:00 +0200

    Remove verbose mode

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ const config = {
   collectCoverageFrom: [ "src/**/*.js" ],
   testEnvironment: "node",
   testMatch: [ "<rootDir>/tests/**/*.js" ],
-  verbose: true,
   watchPathIgnorePatterns: [ "<rootDir>/node_modules/" ]
 }
 
