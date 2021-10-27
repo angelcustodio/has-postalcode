1ff678508f21cf93b9dc02fc26f25812c1af77b7
Angel Custodio <ancude@gmail.com>
2021-10-27 16:12:55 +0200

    Add more tests

--- tests/index.js
+++ tests/index.js
@@ -3,41 +3,69 @@ const noPostalCodeCountries = require('../src/data/nopostal.js')
 
 describe('hasPostalCode', () => {
 
-  describe('should return true when country has postal code', () => {
-    it('with country name', () => {
-      expect(hasPostalCode('Spain')).toBe(true)
+  const countryIs = {
+    included: {
+      name: 'Spain',
+      alpha2: 'ES',
+      alpha3: 'ESP',
+      numeric: 724
+    },
+    notIncluded: {
+      name: 'Tokelau',
+      alpha2: 'TK',
+      alpha3: 'TKL',
+      numeric: 772
+    }
+  }
+
+  describe('when country has postal code', () => {
+    const attrs = countryIs.included
+
+    it('should return true with country name', () => {
+      expect(hasPostalCode(attrs.name)).toBe(true)
     })
-    it('with alpha2 country code', () => {
-      expect(hasPostalCode('ES')).toBe(true)
+    it('should return true with alpha2 country code', () => {
+      expect(hasPostalCode(attrs.alpha2)).toBe(true)
     })
-    it('with alpha3 country code', () => {
-      expect(hasPostalCode('ESP')).toBe(true)
+    it('should return true with alpha3 country code', () => {
+      expect(hasPostalCode(attrs.alpha3)).toBe(true)
     })
-    it('with numeric country code', () => {
-      expect(hasPostalCode(724)).toBe(true)
+    it('should return true with numeric country code', () => {
+      expect(hasPostalCode(attrs.numeric)).toBe(true)
     })
   })
 
-  describe('should return false when country does not have postal code', () => {
-    it('with country name', () => {
-      expect(hasPostalCode('Yemen')).toBe(false)
+  describe('when country does not have postal code', () => {
+    const attrs = countryIs.notIncluded
+
+    it('should return false with country name', () => {
+      expect(hasPostalCode(attrs.name)).toBe(false)
     })
-    it('with alpha2 country code', () => {
-      expect(hasPostalCode('TK')).toBe(false)
+    it('should return false with alpha2 country code', () => {
+      expect(hasPostalCode(attrs.alpha2)).toBe(false)
     })
-    it('with alpha3 country code', () => {
-      expect(hasPostalCode('TKL')).toBe(false)
+    it('should return false with alpha3 country code', () => {
+      expect(hasPostalCode(attrs.alpha3)).toBe(false)
     })
-    it('with numeric country code', () => {
-      expect(hasPostalCode(772)).toBe(false)
+    it('should return false with numeric country code', () => {
+      expect(hasPostalCode(attrs.numeric)).toBe(false)
     })
   })
 
-  describe('should return false for all listed no-postalcode countries', () => {
+  describe('when listed no-postalcode countries', () => {
     noPostalCodeCountries.forEach(country => {
-      it(`like for ${country}`, () => {
+      it(`should return false for ${country}`, () => {
         expect(hasPostalCode(country)).toBe(false)
       })
+    })
+  })
+
+  describe('when a non-compliant iso-3166-1 value is provided', () => {
+    it('should return undefined', () => {
+      expect(hasPostalCode(1)).toBeUndefined()
+      expect(hasPostalCode('troll')).toBeUndefined()
+      expect(hasPostalCode('EZ')).toBeUndefined()
+      expect(hasPostalCode('💩')).toBeUndefined()
     })
   })
 })
