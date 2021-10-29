f8aee8a73a82b930fed49f37691831e3b53c470e
Angel Custodio <angel.custodio@mytheresa.com>
2021-10-29 13:33:35 +0200

    Move jest config to package.json

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  collectCoverageFrom: [ "src/**/*.js" ],
-  testEnvironment: "node",
-  testMatch: [ "<rootDir>/tests/**/*.js" ],
-  watchPathIgnorePatterns: [ "<rootDir>/node_modules/" ]
-}
-
-module.exports = config
